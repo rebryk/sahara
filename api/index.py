3732c9fc39ba967a83d70aff1b99557f798a2f96
Yurii Rebryk <yrebryk@gmail.com>
2022-11-05 23:41:57 -0700

    Add proxy method

--- api/index.py
+++ api/index.py
@@ -1,13 +1,21 @@
+import http
 import flask
 import search
 import generate
-import http
+import requests
 
 from flask import request
 from flask_cors import CORS
 
 app = flask.Flask(__name__)
 cors = CORS(app)
+
+
+HEADERS = {
+    "content-type": "application/json",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+    "x-hasura-api-key": "",
+}
 
 
 @app.route('/search/', methods=["GET"])
@@ -36,6 +44,19 @@ def generate_request():
         response = flask.jsonify(generate.generate_sql(query))
         response.headers["Cache-Control"] = "s-maxage=86400"
         return response
+    except Exception as e:
+        return flask.jsonify({"error": str(e)}), http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+@app.route('/proxy/', methods=["POST"])
+def graphql_proxy():
+    try:
+        data = request.get_json()
+        response = requests.post(
+            "https://core-hsr.dune.com/v1/graphql", json=data, headers=HEADERS
+        )
+        print(response.__dict__)
+        return response.json()
     except Exception as e:
         return flask.jsonify({"error": str(e)}), http.HTTPStatus.INTERNAL_SERVER_ERROR
 
